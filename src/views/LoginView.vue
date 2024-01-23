92207415ab5b4e525c27fc92e853b7d5f173e791
olindikumasabo@gmail.com <olindikumasabo@gmail.com>
2024-01-23 18:45:21 +0200

    init project

---
+++ src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<template>
+   <div>
+    <form action="">
+        <input type="text" v-model="username" name="" id="" placeholder="username">
+        <input type="password" v-model = "password" name="" id="" placeholder="password">
+        <button type="submit" v-on:click="performLogin">Login</button>
+    </form>
+
+    <p>username = {{ username }}</p>
+    <p>password = {{ password }}</p>
+
+    <div>Vos idendifiant sont: <strong>{{ result }}</strong></div>
+   </div>
+
+</template>
+
+<script>
+    export default{
+        data()
+        {
+            return {
+                username:"",
+                password:"",
+                result:""
+            }
+        },
+        methods:{
+            performLogin(){
+                this.result = this.username+ " "+this.password
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
